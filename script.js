9b00198a92ce9549970d6c5b3901c255121eaa08
Paulo Sergio <pauloserg861@gmail.com>
2024-04-12 18:18:31 -0300

    chore: add structure

---
+++ script.js
@@ -0,0 +1,58 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const _city = document.getElementById('city');
+// const search = document.getElementById('search');
+
+// const API_KEY = 'bad44cba85b76dae8835ee1a0e1bfa7f';
+// const city = 'Lisboa';
+// const baseUrl = 'https://api.openweathermap.org/data/2.5/';
+// const path = `weather?q=${city}&units=metric&appid=${API_KEY}&lang=pt_BR`;
+
+// async function main() {
+//   const url = `${baseUrl}${path}`;
+//   const res = await fetch(url)
+//   const data = await res.json();
+
+//   console.log(data)
+// }
+
+// main();
