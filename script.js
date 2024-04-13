32b43c1a0e69354695d04480bc5aaeee639c9f07
Paulo Sergio <pauloserg861@gmail.com>
2024-04-13 10:50:56 -0300

    feat: implement logic to render weather infos

--- script.js
+++ script.js
@@ -1,22 +1,72 @@
-import { environment } from './config/environment.js';
+import { environments } from './config/environment.js';
 
-const { weatherApiKey } = environment;
+const { weatherApiKey } = environments;
 
-console.log(weatherApiKey)
-// const _city = document.getElementById('city');
-// const search = document.getElementById('search');
+const location = document.getElementById('city');
+const search = document.getElementById('search');
+const temp = document.getElementById('temp');
+const cityLabel = document.getElementById('city_label');
 
-// const API_KEY = 'bad44cba85b76dae8835ee1a0e1bfa7f';
-// const city = 'Lisboa';
-// const baseUrl = 'https://api.openweathermap.org/data/2.5/';
-// const path = `weather?q=${city}&units=metric&appid=${API_KEY}&lang=pt_BR`;
+const getGeoLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      console.log(latitude, longitude);
+    })
+  }
+}
 
-// async function main() {
-//   const url = `${baseUrl}${path}`;
-//   const res = await fetch(url)
-//   const data = await res.json();
+const getCityName = () => {
+  const city = location.value;
+  if (!city) {
+    alert('Digite o nome de uma cidade!');
+    return;
+  }
+  showWeatherData(city);
+}
 
-//   console.log(data)
-// }
+search.addEventListener('click', getCityName);
 
-// main();
+const getWeather = async (city) => {
+  const baseUrl = 'https://api.openweathermap.org/data/2.5/';
+  const path = `weather?q=${city}&units=metric&appid=${weatherApiKey}&lang=pt_BR`;
+  const url = `${baseUrl}${path}`;
+
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
+}
+
+const showWeatherData = async (city) => {
+  const weatherData = await getWeather(city);
+  if ("cod" in weatherData && weatherData.cod === "404") {
+    const errorMessage = `A cidade ${location.value} não foi encontrada!`;
+    alert(errorMessage);
+    location.value = '';
+    return;
+  };
+  mountHTML(weatherData);
+}
+
+const mountHTML = (weatherData) => {
+  temp.innerHTML = formatTemp(weatherData.main.temp);
+  cityLabel.innerHTML = weatherData.name;
+}
+
+const formatTemp = (temp) => {
+  const tempFormatted = temp.toFixed(0);
+  return `${tempFormatted}°C`;
+}
+
+function main() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${-3.89065}&lon=${-38.6819}&appid=${weatherApiKey}`
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(err => console.error(err));
+} 
+
+main();
+getGeoLocation();
